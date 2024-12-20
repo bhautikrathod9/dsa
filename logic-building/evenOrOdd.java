089c8e378d8a7fe5be062a413d0555fa8f662061
gogo <hitmangogo7@gmail.com>
2024-12-20 13:19:38 +0530

    two problem solved

---
+++ logic-building/evenOrOdd.java
@@ -0,0 +1,12 @@
+class evenOrOdd{
+
+    public static void main(String[] args){
+       int n = 10;
+
+       if(n%2==0){
+            System.out.println("The given" + n + "is even");
+       }else{
+            System.out.println("The number is odd");
+       }      
+    }
+}
---
+++ logic-building/table.java
@@ -0,0 +1,9 @@
+class table{
+    public static void main(String[] args){
+       int n = 1;
+
+       for(int i=1; i<=10; i++){
+           System.out.println(n + " * " +  i + " = " + n*i);
+       }
+    }
+}
