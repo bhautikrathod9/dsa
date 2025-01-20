3f8bbda9455c45726d14b52f67b58921dbf7fc6a
gogo <hitmangogo7@gmail.com>
2025-01-20 22:55:57 +0530

    dice problem

---
+++ logic-building/diceFace.java
@@ -0,0 +1,24 @@
+// Java program to find opposite face of dice
+class Dice {
+    static int oppositeFaceOfDice(int n) {
+        if (n == 1) {
+            System.out.println(6);
+        } else if (n == 2) {
+            System.out.println(5);
+        } else if (n == 3) {
+            System.out.println(4);
+        } else if (n == 4) {
+            System.out.println(3);
+        } else if (n == 5) {
+            System.out.println(2);
+        } else {
+            System.out.println(1);
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        oppositeFaceOfDice(n);
+    }
+}
