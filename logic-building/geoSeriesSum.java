0c5f5fe22b4b282d4db763aa80596ff7b0e051ef
gogo <hitmangogo7@gmail.com>
2025-01-22 22:46:15 +0530

    efficient solution added in geoSeries sum

--- logic-building/geoSeriesSum.java
+++ logic-building/geoSeriesSum.java
@@ -9,11 +9,18 @@ public class geoSeriesSum {
         return sum;
     }
 
+    //Efficient solution 
+
+    static float sumSeries(float a, float r, float n){
+        return (a * (1 - (int)Math.pow(r, n))) / (1-r);
+    }
+
     public static void main(String[] args) {
         float a = 2;
         float r = (float)(2.0);
         float n = 15;
 
         System.out.printf("Sum: %.2f\n", sumOfseries(a, r, n));
+        System.out.printf("Sum: %.2f\n", sumSeries(a, r, n));
     }
 }
