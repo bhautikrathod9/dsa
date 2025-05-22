670a56ed081c2544b3fa954c95fd1d2351a6f810
Bhautik Rathod <hitmangogo7@gmail.com>
2025-05-22 08:23:15 +0530

    Time: 1 ms (86.32%) | Memory: 40.9 MB (69.65%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+        long low = 1;
+        long high = x/2 + 1;
+
+        while(high - low != 1){
+            long mid = low + (high - low)/2;
+
+            if(mid * mid > x){
+                high = mid;
+            }else{
+                low = mid;
+            }
+        }
+        
+        return (int)low;
+    }
+}
