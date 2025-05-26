ef643cee17e9bcc825978394f83cd9a0f5e7e7b2
Bhautik Rathod <hitmangogo7@gmail.com>
2025-05-26 09:00:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (89.41%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -1,34 +1,43 @@
 class Solution {
     public int diagonalSum(int[][] mat) {
         int sum = 0;
-        int startRow = 0;
-        int endRow = mat.length - 1;
-        int startCol = 0;
-        int endCol = mat[0].length - 1;
 
-        while (startRow <= endRow && startCol <= endCol) {
-            if (startRow == endRow && startCol == endCol) {
-                // Single center element (odd dimensions)
-                sum += mat[startRow][startCol];
-            } else if (startRow == endRow) {
-                // One row left
-                sum += mat[startRow][startCol] + mat[startRow][endCol];
-            } else if (startCol == endCol) {
-                // One column left
-                sum += mat[startRow][startCol] + mat[endRow][startCol];
-            } else {
-                // General case: four distinct corners
-                sum += mat[startRow][startCol] + mat[startRow][endCol] +
-                    mat[endRow][startCol] + mat[endRow][endCol];
+        //approach 1
+        // int startRow = 0;
+        // int endRow = mat.length - 1;
+        // int startCol = 0;
+        // int endCol = mat[0].length - 1;
+
+        // while (startRow <= endRow && startCol <= endCol) {
+        //     if (startRow == endRow && startCol == endCol) {
+        //         // Single center element (odd dimensions)
+        //         sum += mat[startRow][startCol];
+        //     } else if (startRow == endRow) {
+        //         // One row left
+        //         sum += mat[startRow][startCol] + mat[startRow][endCol];
+        //     } else if (startCol == endCol) {
+        //         // One column left
+        //         sum += mat[startRow][startCol] + mat[endRow][startCol];
+        //     } else {
+        //         // General case: four distinct corners
+        //         sum += mat[startRow][startCol] + mat[startRow][endCol] +
+        //             mat[endRow][startCol] + mat[endRow][endCol];
+        //     }
+
+        //     startRow++;
+        //     endRow--;
+        //     startCol++;
+        //     endCol--;
+        // }
+
+        //approach 2
+        for(int i=0; i<mat.length; i++){
+            sum += mat[i][i];
+
+            if(i != mat.length-i-1){
+                sum += mat[i][mat.length-i-1];
             }
-
-            startRow++;
-            endRow--;
-            startCol++;
-            endCol--;
         }
-
-
 
         // for(int i=0; i<mat.length; i++){
         //     for(int j=0; j<mat[0].length; j++){
