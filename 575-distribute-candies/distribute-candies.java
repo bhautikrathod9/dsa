64102218063e7c73bca30aad11443d2791f607ec
Bhautik Rathod <hitmangogo7@gmail.com>
2025-06-08 09:42:38 +0530

    Time: 30 ms (87.05%) | Memory: 45.9 MB (52.25%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : candyType){
+            set.add(i);
+        }
+
+        var n = candyType.length/2;
+
+        if(set.size() >= n){
+            return n;
+        }else{
+            return set.size();
+        }
+    }
+}
