5bcbc4528c685d3f98f2f0701ab266a1834e79c6
Bhautik Rathod <hitmangogo7@gmail.com>
2025-06-27 19:40:45 +0530

    Time: 0 ms (100.00%) | Memory: 46.2 MB (30.06%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,61 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
-        helperfo(nums, 0, target, result);
-        helperlo(nums, nums.length-1, target, result);
+        // int[] result = new int[2];
+        // helperfo(nums, 0, target, result);
+        // helperlo(nums, nums.length-1, target, result);
 
-        return result;
+        // return result;
+
+        int n = nums.length;
+        int firstIndex = -1;
+        int lastIndex = -1;
+        int low = 0;
+        int high = n-1;
+        // to find first position
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > target) high = mid-1;
+            else if(nums[mid] < target) low = mid+1;
+            else {firstIndex = mid; high = mid-1;}
+        }
+        // reassinging 
+        low = 0;
+        high = n-1;
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] > target) high = mid-1;
+            else if(nums[mid] < target) low = mid+1;
+            else {lastIndex = mid; low = mid+1;}
+        }
+
+        return new int[]{firstIndex , lastIndex};
     }
 
-    public void helperfo(int[] nums, int i, int target, int[] result){
-        if(nums.length == i){
-            result[0] = -1;
-            return;
-        }
+    // public void helperfo(int[] nums, int i, int target, int[] result){
+    //     if(nums.length == i){
+    //         result[0] = -1;
+    //         return;
+    //     }
 
-        if(nums[i] == target){
-            result[0] = i;
-            return;
-        }
+    //     if(nums[i] == target){
+    //         result[0] = i;
+    //         return;
+    //     }
 
-        helperfo(nums, i+1, target, result);
-    }
+    //     helperfo(nums, i+1, target, result);
+    // }
 
-    public void helperlo(int[] nums, int i, int target, int[] result){
-        if(i < 0){
-            result[1] = -1;
-            return;
-        }
+    // public void helperlo(int[] nums, int i, int target, int[] result){
+    //     if(i < 0){
+    //         result[1] = -1;
+    //         return;
+    //     }
 
-        if(nums[i] == target){
-            result[1] = i;
-            return;
-        }
+    //     if(nums[i] == target){
+    //         result[1] = i;
+    //         return;
+    //     }
 
-        helperlo(nums, i-1, target, result);
-    }
-
-    
+    //     helperlo(nums, i-1, target, result);
+    // }
 }
